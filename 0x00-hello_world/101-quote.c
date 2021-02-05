693b348956d0a37d6b6e6d4f4a5702bdafee29a7
EugeneJoe <jmuthui70@gmail.com>
2021-02-05 20:05:39 +0000

    Print string to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int write(int filedes, const char *buf, unsigned int nbyte);
+/**
+ * main - Entry point
+ * @argc: number of arguments to main
+ * @argv: pointer to pointer containing addresses of the arguments
+ *
+ * Return: 1
+ */
+int main(int argc, char **argv)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      65);
+
+	return (1);
+}
