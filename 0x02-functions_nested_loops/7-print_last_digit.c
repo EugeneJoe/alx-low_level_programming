561751f2b4e75abc3de9464262e3c9066a0b90a1
EugeneJoe <jmuthui70@gmail.com>
2021-02-08 13:55:59 +0000

    Print the last digit of the passed argument

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @x: number whose last digit is to be printed
+ *
+ * Return: value of the last digit
+ */
+int print_last_digit(int x)
+{
+	int c;
+	if (x < 0)
+		x = x * -1;
+
+	c = x % 10;
+	_putchar(c + '0');
+	return (c);
+}
