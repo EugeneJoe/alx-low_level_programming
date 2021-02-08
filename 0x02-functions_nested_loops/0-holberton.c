f37e5338dfa5b368d9ddebc6207d89ba4c8383ad
EugeneJoe <jmuthui70@gmail.com>
2021-02-08 09:02:51 +0000

    C program that prints Holberton, followed by new line

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * main - call _putchar to print "Holberton".
+ *
+ * Return: 0 (success)
+ */
+int main()
+{
+	char hol[9] = "Holberton";
+	int i;
+
+	i = 0;
+	while(i < 9)
+	{
+		_putchar(hol[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
