b0159f8c4f1299d9f845e554b28105660662e60d
EugeneJoe <jmuthui70@gmail.com>
2021-02-08 19:06:55 +0000

    Returns the sum of two digits passed as arguments

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - returns the result of the sum of the two integers passed as arguments
+ * @x: - one of the integers that's part of the sum
+ * @y: - one of the integers that's part of the sum
+ *
+ * Return: Sum of adding x and y (x + y)
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
