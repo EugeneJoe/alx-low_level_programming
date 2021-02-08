81b94a33b35b0956ad300ea20a1510135485c1bf
EugeneJoe <jmuthui70@gmail.com>
2021-02-08 09:28:08 +0000

    C function that prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints lowercase alphabet letters in ascending order
+ *
+ * no return value
+ */
+void print_alphabet(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	i = 0;
+	while (i < 26)
+		_putchar(alphabet[i++]);
+	_putchar('\n');
+}
