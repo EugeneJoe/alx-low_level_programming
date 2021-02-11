475ae8ddd6164560677e3ac4f67c533a9fc91148
EugeneJoe <jmuthui70@gmail.com>
2021-02-11 09:03:53 +0000

    Print numbers 0 to 9 except 2 and 4, followed by new line

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9, excluding 2 and 4
+ * Return: no return value
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (x == 2 || x == 4)
+			continue;
+		else
+			_putchar(x + '0');
+	}
+	_putchar('\n');
+}
