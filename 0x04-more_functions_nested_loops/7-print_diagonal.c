1efb3fb44e2978f322dd7ea529fde75078e3c0c1
EugeneJoe <jmuthui70@gmail.com>
2021-02-11 12:03:17 +0000

    Print diagonal line on terminal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_line - prints _  n times to make a straight line, followed by new line
+ * @n: number of times to print _
+ * Return: no return value
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	y = 0;
+	if (n > 0)
+	{
+		for (x = 1; x <= n; x++)
+		{
+			y = x - 1;
+			while (y > 0)
+			{
+				_putchar(' ');
+				y--;
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
