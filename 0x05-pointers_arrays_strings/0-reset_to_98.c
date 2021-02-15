994472ce9f632ee96c2a6675f0821f8fd71bc206
EugeneJoe <jmuthui70@gmail.com>
2021-02-15 08:48:06 +0000

    Edit file to conform to betty coding style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * rest_to_98 - resets the value of a variable whose address is passed to it
-through a pointer to 98.
+ * reset_to_98 - resets the value of a variable whose address is passed to it
+ * through a pointer to 98.
  * @n: pointer containing the address of the variable to be reset.
  * Return: no return value
  */
