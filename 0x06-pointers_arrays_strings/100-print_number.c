3cbebeb8c5d76afd8d7ce4433aaa1880e3620c91
EugeneJoe <jmuthui70@gmail.com>
2021-02-18 20:35:10 +0000

    Use unsigned int to handle larger numbers

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -8,12 +8,14 @@
  */
 void print_number(int n)
 {
+	unsigned int num = 0;
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	num = n;
+	if (num / 10)
+		print_number(num / 10);
+	_putchar(num % 10 + '0');
 }
