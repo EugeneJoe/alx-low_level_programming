408dc2bd96562d3d08722f54e74a0484e6ed9126
EugeneJoe <jmuthui70@gmail.com>
2021-02-18 13:02:44 +0000

    Edit code to conform to betty coding style

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,9 +16,7 @@ char *_strncpy(char *dest, char *src, int n)
 	while (*(dest + dlen) != '\0')
 		dlen++;
 	for (count = 0; count < n && *(src + count) != '\0'; count++)
-	{
-			*(dest + count) = *(src + count);
-	}
+		*(dest + count) = *(src + count);
 	while (count < n)
 	{
 		*(dest + count) = '\0';
