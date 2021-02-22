12e0aabde009568a6646d0c0f137f49eedcff8a3
EugeneJoe <jmuthui70@gmail.com>
2021-02-22 14:35:13 +0000

    Prints a chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_chessboard - prints a chessboard, using the 1st character of each piece
+ * name to represent the chess piece
+ * @a: 2D array containing the chessboard characters
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int n, m, len, rows;
+
+	len = 0;
+	while (*((*a) + len) != 0)
+		len++;
+	rows = len / 8;
+	m = 0;
+	while (m < rows)
+	{
+		for (n = 0; n < 8; n++)
+			_putchar(a[m][n]);
+		_putchar('\n');
+		m++;
+		}
+
+}
