7528afa753a043c1da8e0cb2ce2637f5198fa4d9
EugeneJoe <jmuthui70@gmail.com>
2021-02-24 09:44:26 +0000

    Determines the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to a string whose length is to be found
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*(s))
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
