a78fcac93af481a785873ba564e532b6e9b2e0ad
EugeneJoe <jmuthui70@gmail.com>
2021-03-08 19:10:05 +0000

    Free dynamically created variable dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dog variable that was dynamically created
+ * @d: pointer to dog variable to be freed
+ *
+ * Return: no retunr value (void)
+ */
+void free_dog(dog_t *d)
+{
+	if (d != 0)
+	{
+		free(d);
+	}
+	else
+	{
+		return;
+	}
+}
