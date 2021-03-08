3a8a78703736b9e19b883da9ee4b52f58bb8f406
EugeneJoe <jmuthui70@gmail.com>
2021-03-08 19:38:32 +0000

    Print struct dog varaible's values

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,14 +9,19 @@
  */
 void print_dog(struct dog *d)
 {
+	char *nil = "(nil)";
+
 	if (d != NULL)
 	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %1.6f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		printf("Name: %s\n", d->name == 0 ? nil : d->name);
+		if (d->age != 0)
+			printf("Age: %1.6f\n", d->age);
+		else
+			printf("Age: %s\n", nil);
+		printf("Owner: %s\n", d->owner == 0 ? nil : d->owner);
 	}
 	else
 	{
-		exit (98);
+		return;
 	}
 }
