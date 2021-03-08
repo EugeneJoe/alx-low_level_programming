147faf2a875dbbf8d42ef56e75fee72a44cb7052
EugeneJoe <jmuthui70@gmail.com>
2021-03-08 17:38:48 +0000

    Check whether pointer is NULL

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints the contents of a struct dog
+ * @d: pointer to the struct to be printed
+ *
+ * Return: no return value
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %1.6f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
