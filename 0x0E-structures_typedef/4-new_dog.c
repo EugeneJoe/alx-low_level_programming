3adc973f33dc0e1cb0767e452bb897309ce9be4b
EugeneJoe <jmuthui70@gmail.com>
2021-03-08 20:41:26 +0000

    Allocate space for strings in struct dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,16 +12,42 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *mydog;
+	int lenname, lenowner;
 
+	lenname = 0;
+	while (*(name + lenname) != '\0')
+		lenname++;
+	lenowner = 0;
+	while (owner[lenO] != '\0')
+		lenO++;
 	mydog = malloc(sizeof(dog_t));
 	if (mydog == 0)
 	{
 		free(mydog);
 		return (0);
 	}
-	mydog->name = name;
+
+	mydog->name = malloc(lenname + 1);
+	if (mydog->name == 0)
+	{
+		free(mydog->name);
+		free(mydog);
+		return (0);
+	}
+
+	mydog->owner = malloc(lenowner + 1);
+	if (mydog->owner == 0)
+	{
+		free(mydog->owner);
+		free(mydog->name);
+		free(mydog);
+		return (0);
+	}
+	for (i = 0; i <= lenname; i++)
+		mydog->name[i] = name[i];
+	for (i = 0; i <= lenowner; i++)
+		mydog->owner[i] = owner[i];
 	mydog->age = age;
-	mydog->owner = owner;
 
 	return (mydog);
 }
