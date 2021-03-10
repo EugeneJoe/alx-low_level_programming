c2e07b132aae345c453c28364981b39bfe17ccf0
EugeneJoe <jmuthui70@gmail.com>
2021-03-10 13:14:39 +0000

    Selects correct function to use for selected operation

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * get_op_func - selects the correct function to perform the operation selected
+ * by the user.
+ * @s: char representing operation selected by the user.
+ *
+ * Description: This function returns a pointer to a function that takes
+ * 2 integer arguments and returns an integer. It does so by comparing
+ * character s to either +,-,*,/ or % to determine the required function
+ *
+ * Return: pointer to function on success, NULL if s does not match expected
+ * operator characters.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 6)
+	{
+		if (*s == *ops[i].op)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
