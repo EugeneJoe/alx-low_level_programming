1205fa4b0116461c48e1c25457a436bca397d204
EugeneJoe <jmuthui70@gmail.com>
2021-03-11 22:26:27 +0000

    Prints anything to the screen

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,72 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * _strlen - finds the length of as string passed to it
+ * @s: string whose length is to be found
+ * Return: length of the string
+ */
+int _strlen(const char * const s)
+{
+	int x = 0;
+
+	while (*(s + x) != '\0')
+		x++;
+	return (x);
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of the types of arguments passed to the function
+ *
+ * Description: Takes in variable number and type of arguments. Format is a
+ * string that contains format specifiers for the arguments passed to the
+ * function.
+ * c - char, i - integer, f - float, s - char*(string)
+ * If a NULL string is passed, print '(nil)' instead.
+ *
+ * Return: no return value
+ */
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	unsigned int i, num, len;
+	char c;
+	double d;
+	char *s;
+
+	if (format == NULL)
+		return;
+	len = _strlen(format);
+	va_start(list, format);
+	i = 0;
+	while (i < len)
+	{
+		switch (format[i])
+		{
+		case 'c':
+			c = va_arg(list, int);
+			printf("%c", c);
+			break;
+		case 'i':
+			num = va_arg(list, int);
+			printf("%d", num);
+			break;
+		case 'f':
+			d = va_arg(list, double);
+			printf("%f", d);
+			break;
+		case 's':
+			s = va_arg(list, char *);
+			if (s != NULL)
+				printf("%s", s);
+			break;
+		}
+		if (i < len - 1)
+			printf(", ");
+		if (i == len - 1)
+			printf("\n");
+		i++;
+	}
+	va_end(list);
+}
