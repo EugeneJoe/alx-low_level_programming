7c344e924acdec98b886eebeedf44f6ed4709b70
EugeneJoe <jmuthui70@gmail.com>
2021-03-23 20:11:27 +0000

    Rewrite if statement

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,10 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	else
-	{
-		new->next = current;
-		previous->next = new;
-		return (new);
-	}
+	new->next = current;
+	previous->next = new;
+	return (new);
 }
