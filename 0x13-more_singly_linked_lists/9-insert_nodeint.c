9f2f6a66a49bec3cb7362dd19bd77a10c7be6d73
EugeneJoe <jmuthui70@gmail.com>
2021-03-23 20:49:09 +0000

    Rewrite NULL checks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,8 +12,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *previous, *current;
 
-	if ((*head == NULL || head == NULL) && idx != 0)
-		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 	{
