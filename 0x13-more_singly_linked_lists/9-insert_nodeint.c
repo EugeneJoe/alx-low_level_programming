07c85d06a301554a8a27c9d480788cfe4c9f9efb
EugeneJoe <jmuthui70@gmail.com>
2021-03-24 20:56:53 +0000

    Set pointer to NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,6 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (current == NULL)
 	{
 		free(new);
+		previous = NULL;
 		return (NULL);
 	}
 	new->next = current;
