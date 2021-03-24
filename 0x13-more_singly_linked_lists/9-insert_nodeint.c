643f7a36629fb11c5d70c0356c405969dcda429d
EugeneJoe <jmuthui70@gmail.com>
2021-03-24 20:54:43 +0000

    Set pointers back to NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,6 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new->next = current;
 	previous->next = new;
-	free(current);
+	current = NULL;
+	previous = NULL;
 	return (new);
 }
