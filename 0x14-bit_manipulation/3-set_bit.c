5d0f1b8f160917ec176f507450d8f3e1c42380fa
EugeneJoe <jmuthui70@gmail.com>
2021-03-25 12:49:24 +0000

    Sets bit at index position to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: pointer to number whose bit is to be set
+ * @index: index of bit to be set to 1
+ *
+ * Return: 1 on Success, -1, if an error occured
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int *nn;
+
+	if (n)
+	{
+		nn = malloc(sizeof(unsigned long int));
+		if (nn == NULL)
+		{
+			free(nn);
+			return (-1);
+		}
+		*nn = 1;
+		while (index > 0)
+		{
+			*nn <<= 1;
+			index--;
+		}
+		*n = *n | *nn;
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+}
