3fdfacc4e3ac0df165fa8d3f7e6eb977d3595e2b
EugeneJoe <jmuthui70@gmail.com>
2021-03-25 17:46:59 +0000

    Check whether index is greater that number of bits

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,6 +11,8 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask = 1 << index;
 
+	if (index > (sizeof(unsigned long int) * 8))
+		return (-1);
 	if (n)
 	{
 		*n = ((*n & ~mask) | (1 << index));
