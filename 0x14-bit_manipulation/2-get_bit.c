d804708903c7a02a8181100928c9d198616754ff
EugeneJoe <jmuthui70@gmail.com>
2021-03-25 17:05:45 +0000

    Check for n being 0

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,6 +9,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (n == 0 && index == 0)
+		return (0);
+	else if (n == 0 && index != 0)
+		return (-1);
 	while (index > 0 && n != 0)
 	{
 		n >>= 1;
