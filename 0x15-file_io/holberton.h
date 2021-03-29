3450019419a616678414c18a0c3bd6265fda5ab8
EugeneJoe <jmuthui70@gmail.com>
2021-03-29 19:14:12 +0000

    Update prototypes

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -12,6 +12,8 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 void update_file(const char *filename, char *text_content);
+void new_file(const char *filename, char *text_content);
+void read_error(const char *file);
 int append_text_to_file(const char *filename, char *text_content);
 void cp(const char *file_from, const char *file_to);
 
