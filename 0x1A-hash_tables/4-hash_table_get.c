c31b10448aaa88d1933e2818e6224b96f7c67535
EugeneJoe <jmuthui70@gmail.com>
2021-05-28 16:33:08 +0000

    Simplify while condition

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,15 +14,18 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = 0;
 	hash_node_t *temp = NULL;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
 	index = key_index((unsigned char *)key, ht->size);
 	temp = ht->array[index];
-	if (temp->key == NULL)
-		return (NULL);
-	while (strcmp(temp->key, key) != 0 && temp != NULL)
-		temp = temp->next;
 
-	if (temp != NULL)
-		return (temp->value);
-	else
-		return (NULL);
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+
+	return (NULL);
 }
