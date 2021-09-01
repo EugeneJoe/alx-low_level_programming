e294f4ff9cfb261deefbe0713b835e51fb4a73ee
EugeneJoe <jmuthui70@gmail.com>
2021-09-01 16:31:27 +0000

    Update

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -2,54 +2,27 @@
 #include <stdlib.h>
 #include "search_algos.h"
 
-
 /**
- * binary_recurse - recursive function implementing binary search
- * @array: pointer to first element of the array to search in
- * @left: lefter boundary of search space to look in
- * @right: upper boundary of search space to look in
- * @res: stores current result, could be -1 if match hasn't been found yet
- * @value: value to search for in the array
+ * display_array - prints an array of integers
+ * @array: array to print
+ * @size: size of array
  *
- * Return: index where value is located, else -1
+ * return: void
  */
-int binary_recurse(int *array, size_t left, size_t right, int res, int value)
+void display_array(int *array, size_t size)
 {
-	size_t idx = 0;
-	int mid = 0;
+	size_t i;
 
-	if (left <= right)
+	printf("Searching in array:");
+	for (i = 0; i < size; i++)
 	{
-		printf("Searching in array: ");
-		for (idx = left; idx <= right; idx++)
-		{
-			printf("%d", array[idx]);
-			if (idx != right)
-				printf(", ");
-			else
-				printf("\n");
-		}
-		mid = (left + right) / 2;
-		if (array[mid] < value)
-		{
-			left = mid + 1;
-			return (binary_recurse(array, left, right, res, value));
-		}
-		else if (array[mid] > value)
-		{
-			right = mid - 1;
-			return (binary_recurse(array, left, right, res, value));
-		}
-		else
-		{
-			res = mid;
-			right = mid - 1;
-			return (binary_recurse(array, left, right, res, value));
-		}
+		printf(" %d", array[i]);
+		if (i != size - 1)
+			printf(",");
 	}
-
-	return (res);
+	printf("\n");
 }
+
 
 /**
  * advanced_binary - searches for value in a sorted array of integers using the
@@ -62,16 +35,37 @@ int binary_recurse(int *array, size_t left, size_t right, int res, int value)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t left = 0, right = size - 1;
-	/*
-	 * res stores current result to be passed recursively
-	 * allows us to keep searching for left-most matching value to find
-	 * first occurence of value
-	 */
-	int res = -1;
+	size_t mid = 0;
+	int res = -1, val = 0;
 
-	if (array == NULL)
+	if (array == NULL || size < 1)
+	{
 		return (-1);
+	}
 	else
-		return (binary_recurse(array, left, right, res, value));
+	{
+		display_array(array, size);
+		mid = (size - 1) / 2;
+		val = array[mid];
+		if (val < value)
+		{
+			res = advanced_binary(array + mid + 1, size - (mid + 1),
+					      value);
+			if (res >= 0)
+				res += mid + 1;
+		}
+		else if (val > value)
+		{
+			res = advanced_binary(array, mid + 1, value);
+		}
+		else if (mid == 0 || array[mid - 1] < val)
+		{
+			res = mid;
+		}
+		else
+		{
+			res = advanced_binary(array, mid + 1, value);
+		}
+	}
+	return (res);
 }
