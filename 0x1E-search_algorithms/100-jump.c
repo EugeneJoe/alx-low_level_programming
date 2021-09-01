03261c632229c5d268dd6ad016c21f0e4c2aff1a
EugeneJoe <jmuthui70@gmail.com>
2021-09-01 10:21:20 +0000

    Check whether prev > size

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -27,13 +27,19 @@ int jump_search(int *array, size_t size, int value)
 		step += root;
 	}
 
-	printf("Value found between indexes [%lu] and [%lu]\n", prev, step);
-
-	for (idx = prev; idx < size; idx++)
+	if (prev < size)
 	{
-		printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
-		if (array[idx] == value)
-			return (idx);
+		printf("Value found between indexes [%lu] and [%lu]\n",
+		       prev, step);
+
+		for (idx = prev; idx < size; idx++)
+		{
+			printf("Value checked array[%lu] = [%d]\n",
+			       idx, array[idx]);
+
+			if (array[idx] == value)
+				return (idx);
+		}
 	}
 
 	return (-1);
